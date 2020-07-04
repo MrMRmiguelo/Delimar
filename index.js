0d099ee82c816411a2d645fc5228e4a4953bf5eb
Jorge Sanchez <js7170206@gmail.com>
2020-07-04 12:18:36 -0600

    Creacion y conexion con la base de datos mediante la utilizacion de promesas

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 // Importar los modulos de express.js
-
 const express = require("express");
 
 //Importar todas las rutas de routes
 const routes = require("./routes");
 
-// App de express
+// Conexion con la base de dattos MYSQL
+const db = require("./config/db");
+// Conexion mediante promesas
+db.sync()
+     .then(() => console.log("Connected to the database server"))
+     .catch((error) => console.log(error));
 
+// App de express
 const app = express();
 
 //Rutas para el servidor
 app.use("/", routes());
 
 // Inicializar
-
 app.listen(7000, () => {
      console.log("Server running on port 7000");
 });
