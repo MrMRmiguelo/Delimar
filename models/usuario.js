993abc8bafcf2d193bc9804afec44b801ffc9fb0
Jorge Sanchez <js7170206@gmail.com>
2020-07-07 00:49:05 -0600

    Encriptacion de la contraseña del usuario

--- models/usuario.js
+++ models/usuario.js
@@ -4,44 +4,57 @@ const db = require("../config/db");
 const bcrypt = require("bcrypt-nodejs");
 
 // Crear el modelo para los Usuarios
-const Usuario = db.define("usuario", {
-    id:{
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
+const Usuario = db.define("usuario",
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    user:{
-        type: Sequelize.STRING(50),
-        allowNull: false,
-        unique: {
-            args: true,
-            msg: "Ya existe este nombre de usuario :("
+    fullname: {
+      type: Sequelize.STRING(100),
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: "Debes ingresar tu nombre completo :)",
         },
-        validate: {
-            notEmpty: {
-                msg: "Debes ingresar un nombre de usuario :)"
-            }
-        }
+      },
     },
-    password:{
-        type: Sequelize.STRING(50),
-        allowNull: false,
-        validate:{
-            notEmpty: {
-                msg: "Debes ingresar una contraseña :)"
-            }
-        }
+    user: {
+      type: Sequelize.STRING(50),
+      allowNull: false,
+      unique: {
+        args: true,
+        msg: "Ya existe este nombre de usuario :(",
+      },
+      validate: {
+        notEmpty: {
+          msg: "Debes ingresar un nombre de usuario :)",
+        },
+      },
     },
-    fullname:{
-        type: Sequelize.STRING(100),
-        allowNull: false,
-        validate:{
-            notEmpty:{
-                msg: "Debes ingresar tu nombre completo :)"
-            }
-        }
-    }
-});
+    password: {
+      type: Sequelize.STRING(100),
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: "Debes ingresar una contraseña :)",
+        },
+      },
+    },
+  },
+  {
+    hooks: {
+      beforeCreate(usuario) {
+        usuario.password = bcrypt.hashSync(
+          usuario.password,
+          bcrypt.genSaltSync(13)
+        );
+      },
+    },
+  }
+);
 
 // Exportar el modulo
 module.exports = Usuario;
+
